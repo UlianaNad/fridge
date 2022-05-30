fb0ae70995691381c8fa368fe520d01df52c06f0
”John <102276922+UlianaNad@users.noreply.github.com>
2022-05-30 13:25:32 +0300

    new commit

---
+++ script/main.js
@@ -0,0 +1,36 @@
+const elSalary = document.querySelector('select[name="salary"]');
+const elShopping = document.querySelector('select[name="shopping"]');
+const elStatus = document.querySelector('.status > span');
+
+const run = () => {
+    const valueSalary = elSalary.value;
+    const valueSpopping = elShopping.value;
+
+    if(valueSalary === 'no') {
+        elStatus.innerHTML = "You don't have money";
+        return;
+    }
+
+    if(valueSalary === 'yes') {
+        elStatus.innerHTML = "You have money";
+        return;
+    }
+
+    if(valueSpopping === 'bought') {
+        elStatus.innerHTML = "You have sosage in the frige. Have a good meel!";
+        return;
+    }
+
+    if(valueSpopping === 'forgot') {
+        elStatus.innerHTML = "Sleep hungry stupid ass!!!";
+        return;
+    }
+
+}
+
+run();
+
+elSalary.addEventlistener('change', run);
+elShopping.addEventlistener('change', run);
+
+
